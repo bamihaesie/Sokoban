e431aefcac0bef8c1d6bda396d83cb78485101ea
Bogdan Amihaesie <bogdan.am6@gmail.com>
2011-11-20 16:26:21 +0000

    basic graph

--- src/main/java/com/bogdan/sokoban/ai/Graph.java
+++ src/main/java/com/bogdan/sokoban/ai/Graph.java
@@ -1,10 +1,88 @@
 package com.bogdan.sokoban.ai;
 
+import java.io.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * @author Bogdan
  */
 public class Graph {
 
+    private Set<Node> vertices;
+    private Node start, finish;
+    private HashMap<Node, List<Node>> edges;
 
+    public Graph(File inputFile) {
+        vertices = new HashSet<Node>();
+        readFromFile(inputFile);
+    }
+
+    private void readFromFile(File inputFile) {
+        try {
+            BufferedReader bufferedReader =
+                    new BufferedReader(new FileReader(inputFile));
+            String line = bufferedReader.readLine();
+            int count = Integer.parseInt(line);
+            int index = 0;
+            Node[][] nodeMatrix = new Node[count][count];
+            while((line = bufferedReader.readLine()) != null) {
+                String[] tokens = line.split(" ");
+                for (int i=0; i<tokens.length; i++) {
+                    int nodeType = Integer.parseInt(tokens[i]);
+                    Node node = new Node(nodeType);
+                    nodeMatrix[index][i] = node;
+                    vertices.add(node);
+                    if (nodeType == 1) {
+                        start = node;
+                    } else if (nodeType == 2) {
+                        finish = node;
+                    }
+                }
+            }
+
+        } catch (FileNotFoundException fnfe) {
+            System.err.println("Input file not found!");
+        } catch (IOException ioe) {
+            System.err.println("Error reading input file!");
+        }
+    }
+
+    public Set<Node> getVertices() {
+        return vertices;
+    }
+
+    public Node getStart() {
+        return start;
+    }
+
+    public Node getFinish() {
+        return finish;
+    }
+}
+
+class Node {
+
+    NodeType type;
+
+    public Node(NodeType type) {
+        this.type = type;
+    }
+}
+
+enum NodeType {
+
+    EMPTY (0),
+    START (1),
+    FINISH (2),
+    WALL (3);
+
+    private int code;
+
+    NodeType(int code) {
+        this.code = code;
+    }
 
 }
--- src/test/java/com/bogdan/sokoban/ai/GraphTest.java
+++ src/test/java/com/bogdan/sokoban/ai/GraphTest.java
@@ -1,17 +1,43 @@
 package com.bogdan.sokoban.ai;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import java.io.File;
+import java.net.URL;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * @author Bogdan
  */
 public class GraphTest {
 
+    private Graph graph;
+
+    @Before
+    public void setUp() {
+        URL url = this.getClass().getResource("/graph1.txt");
+        assertNotNull("Can't open input file!", url);
+        File graphFile = new File(url.getFile());
+        graph = new Graph(graphFile);
+    }
+
     @Test
-    public void firstTest() {
-        assertTrue(true);
+    public void readFromFileTest() {
+        assertNotNull(graph.getVertices());
+        assertEquals(25, graph.getVertices().size());
+    }
+
+    @Test
+    public void startIsSetTest() {
+        assertNotNull(graph.getStart());
+    }
+
+    @Test
+    public void finishIsSetTest() {
+        assertNotNull(graph.getFinish());
     }
 
 }
